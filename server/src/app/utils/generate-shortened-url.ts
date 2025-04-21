f0142194ffd6e734a32dec08c4632d56e13356d2
Murilo <murilo.pereira.botelho@gmail.com>
2025-04-21 03:32:19 -0300

    feat: readme update

--- server/src/app/utils/generate-shortened-url.ts
+++ server/src/app/utils/generate-shortened-url.ts
@@ -8,11 +8,13 @@ const SHORTENED_URL_PREFIX = "brev.ly";
 // A bit overengineered, but should guarantee a unique shortened url
 // with no performance degradation
 export function generateShortenedUrl(originalUrl: string) {
+  // md5 is URL-friendly
   const urlHash = createHash("md5")
     .update(originalUrl)
     .digest("hex")
     .substring(0, HASH_LENGTH);
 
+  // nanoid is URL-friendly
   const randomChars = nanoid(RANDOM_LENGTH);
 
   return `https://${SHORTENED_URL_PREFIX}/${urlHash}${randomChars}`;
